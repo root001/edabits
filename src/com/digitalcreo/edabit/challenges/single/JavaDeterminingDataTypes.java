b297c1b69ca476346827d1086246318b0e0d4f31
Abdul-Basit Adebayo <abdulbazit@yahoo.ca>
2021-01-02 11:07:40 +0100

    Java Loops and datatypes

---
+++ src/com/digitalcreo/edabit/challenges/single/JavaDeterminingDataTypes.java
@@ -0,0 +1,29 @@
+package com.digitalcreo.edabit.challenges.single;
+
+import java.util.Scanner;
+
+public class JavaDeterminingDataTypes {
+    public void WhichDataType() {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            try {
+                long x = sc.nextLong();
+                System.out.println(x + " can be fitted in:");
+                if (x >= -128 && x <= 127) System.out.println("* byte");
+                //Complete the code
+                if (x >= Math.pow(-2, 15) && x <= Math.pow(2, 15)-1) System.out.println("* short");
+                if (x >= Math.pow(-2, 31) && x <= Math.pow(2, 31)-1) System.out.println("* int");
+                if (x >= Math.pow(-2, 63) && x <= Math.pow(2, 63)-1) System.out.println("* long");
+            } catch (Exception e) {
+                System.out.println(sc.next() + " can't be fitted anywhere.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        JavaDeterminingDataTypes out = new JavaDeterminingDataTypes();
+        //    System.out.println(0 + (Math.pow(2, 0) * 2));
+        out.WhichDataType();
+    }
+}
--- src/com/digitalcreo/edabit/challenges/single/javaLoopsII.java
+++ src/com/digitalcreo/edabit/challenges/single/javaLoopsII.java
@@ -1,23 +1,22 @@
 package com.digitalcreo.edabit.challenges.single;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class javaLoopsII {
-    public void series(){
+    public void series() {
         Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        for(int i=0;i<t;i++){
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
             int a = in.nextInt();
             int b = in.nextInt();
             int n = in.nextInt();
 
             int count = 0;
             int c = a;
-            while(count < n){
+            while (count < n) {
                 c += (Math.pow(2, count) * b);
                 count++;
-                System.out.printf("%s ",c);
+                System.out.printf("%s ", c);
             }
             System.out.println();
         }
@@ -26,7 +25,7 @@ public class javaLoopsII {
 
     public static void main(String[] args) {
         javaLoopsII out = new javaLoopsII();
-    //    System.out.println(0 + (Math.pow(2, 0) * 2));
+        //    System.out.println(0 + (Math.pow(2, 0) * 2));
         out.series();
     }
 }
